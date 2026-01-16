3b810fde92f441e7c3725a801c78451ece13b2db
nghia.huynh <nghia.huynh@vikki.vn>
2026-01-16 16:31:58 +0700

    update ignore

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket       = "kai-eks-demo-terraform-state"
+    bucket       = "eks-demo-terraform-state"
     key          = "prod/act-workload-prod.tfstate"
     region       = "ap-southeast-1"
     use_lockfile = true
